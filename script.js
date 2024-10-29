55b09ebf07592d7b2134db6dff76d20a0bb252a6
Sanni-Singh <aryansingh7371@gmail.com>
2024-10-29 18:55:28 +0530

    added

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const h1 = document.querySelector('h1');
 const p = document.querySelector('p');
 
 addEventListener('keydown',(e)=>{
-    h1.innerText = `You Have Prassed ${e.key}`
+    h1.innerText = e.key === " "?`You Have Prassed Space`:`You Have Prassed ${e.key}`
     p.innerText = e.keyCode;
     console.log(e);
     
